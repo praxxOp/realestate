fcf4e6c4918fa4f27422c2051ee89f4f77651908
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-05 16:55:56 +0530

    modified:   src/app/LayoutComponents/Navbar/Navbar.css 	modified:   src/app/LayoutComponents/Navbar/Navbar.jsx 	modified:   src/app/PageComponents/Home/Home.css 	modified:   src/app/PageComponents/Home/Home.jsx 	modified:   src/app/globals.css

--- src/app/LayoutComponents/Navbar/Navbar.jsx
+++ src/app/LayoutComponents/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
+import { goldman, michroma } from "@/app/font";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className={` ${goldman.className} main__navbar`}>
+        <h4>REAL ESTATES</h4>
+        <div className="mid__nav">
+          <h4>Buy House</h4>
+          <h4>Rent House</h4>
+        </div>
+        <h4>about-us</h4>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/app/PageComponents/Home/Home.jsx
+++ src/app/PageComponents/Home/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
+import { delagothicone, goldman, michroma, sora } from "@/app/font";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="main__home">
+        <div className="headings__home">
+          <div className="button">
+            <h4 className={goldman.className}>EXPLORE</h4>
+          </div>
+          <h1 className={michroma.className}>
+            Prime <sup>®</sup>
+          </h1>
+          <h1 className={delagothicone.className}>Properties</h1>
+        </div>
+        <p className={sora.className}>
+          "Unlock Dreams: Elevate your lifestyle with our premier real estate
+          solutions at Epic Estates. Discover exceptional properties and turn
+          your aspirations into reality."
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
