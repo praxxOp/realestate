9bffe0c368c2f245023516af8df4cee81b76d1bd
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-05 14:54:30 +0530

    modified:   src/app/layout.js 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { delagothicone, goldman } from "./font";
+import Home from "./PageComponents/Home/Home";
+import Property from "./PageComponents/Property/Property";
+import Portfolio from "./PageComponents/Portfolio/Portfolio";
+import About from "./PageComponents/About/About";
 
 const Page = () => {
-  return <div className={delagothicone.className}>hi</div>;
+  return (
+    <>
+    <Home/>
+    <Property/>
+    <Portfolio/>
+    <About/>
+    </>
+  );
 };
 
 export default Page;
