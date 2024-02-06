6a7c1500e5276ed400b4791ad3d1152a7dd6333b
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-06 18:51:33 +0530

    new file:   src/app/Goldman-Regular.ttf 	modified:   src/app/PageComponents/Home/Home.css 	modified:   src/app/font.js

--- src/app/font.js
+++ src/app/font.js
@@ -1,8 +1,12 @@
-import { Goldman, Michroma , Dela_Gothic_One, Sora} from "next/font/google";
 
-export const goldman = Goldman({
+
+
+import { Michroma, Dela_Gothic_One, Sora } from "next/font/google";
+import localFont from "next/font/local";
+
+export const goldman = localFont({
+  src: "./Goldman-Regular.ttf",
   weight: "400",
-  subsets: ["latin"],
   display: "swap",
 });
 
@@ -13,13 +17,13 @@ export const michroma = Michroma({
 });
 
 export const delagothicone = Dela_Gothic_One({
-    weight: "400",
+  weight: "400",
   subsets: ["latin"],
   display: "swap",
-})
+});
 
 export const sora = Sora({
-    subsets: ["latin"],
-    display: "swap",
-    weight: ["400", "500", "600"],
-  });
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600"],
+});
