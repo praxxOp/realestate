23a94a447929e3742268a4185e58900ba67f7a24
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-08 21:06:53 +0530

    modified:   src/app/PageComponents/Property/Property.css 	modified:   src/app/PageComponents/Property/Property.jsx

--- src/app/PageComponents/Property/Property.jsx
+++ src/app/PageComponents/Property/Property.jsx
@@ -1,21 +1,41 @@
+"use client";
+
 import React from "react";
 import "./Property.css";
 import Image from "next/image";
 import { MagnoSans, goldman } from "@/app/font";
+import { motion } from "framer-motion";
 
 const Property = () => {
   return (
     <>
       <div className="main__property">
         <div className="property__top__info">
-          <div className="property__top__image">
+          <motion.div
+            initial={{
+              height: "0%",
+            }}
+            whileInView={{
+              height: "100%",
+              transition: {
+                type: "spring",
+                duration: 3,
+                stiffness:20
+              },
+            }}
+            viewport={{
+              once: true,
+              margin: "-200px",
+            }}
+            className="property__top__image"
+          >
             <Image
               src="/buy.png"
               width={400}
               height={400}
               alt="Image of a house"
             />
-          </div>
+          </motion.div>
           <div className="property__top__description">
             <div className={` property__top__p`}>
               Discover your dream home in our 'Buy' section. From urban condos
