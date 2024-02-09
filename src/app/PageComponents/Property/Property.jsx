7ba1651a76799f3cd2ec1499ef99451b030f4b50
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-09 13:19:12 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   public/rent2building.jpg 	modified:   src/app/PageComponents/Property/Property.css 	modified:   src/app/PageComponents/Property/Property.jsx

--- src/app/PageComponents/Property/Property.jsx
+++ src/app/PageComponents/Property/Property.jsx
@@ -5,6 +5,7 @@ import "./Property.css";
 import Image from "next/image";
 import { MagnoSans, goldman } from "@/app/font";
 import { motion } from "framer-motion";
+import { GoArrowUpRight } from "react-icons/go";
 
 const Property = () => {
   return (
@@ -20,7 +21,7 @@ const Property = () => {
               transition: {
                 type: "spring",
                 duration: 3,
-                stiffness:20
+                stiffness: 20,
               },
             }}
             viewport={{
@@ -42,12 +43,57 @@ const Property = () => {
               to suburban houses, find the perfect property for you. Start your
               journey today!
             </div>
+
             <div className={`  property__top__button`}>
               <h4 className={goldman.className}>BUY HOUSE</h4>
+              <div className="arrow">
+                <GoArrowUpRight size={30} />
+              </div>
             </div>
           </div>
         </div>
+        <div className="property__bottom__info">
+          <div className="property__bottom__description">
+            <div className={` property__bottom__p`}>
+              Discover your dream home in our 'Buy' section. From urban condos
+              to suburban houses, find the perfect property for you. Start your
+              journey today!
+            </div>
+            <div className="property__bottom__button">
+              <h4 className={goldman.className}>RENT HOUSE</h4>
+              <div className="arrow">
+                <GoArrowUpRight size={30} />
+              </div>
+            </div>
+          </div>
+          <motion.div
+            initial={{
+              height: "0%",
+            }}
+            whileInView={{
+              height: "100%",
+              transition: {
+                type: "spring",
+                duration: 3,
+                stiffness: 20,
+              },
+            }}
+            viewport={{
+              once: true,
+              margin: "-200px",
+            }}
+            className="property__bottom__img"
+          >
+            <Image
+              src={"/rent2building.jpg"}
+              width={400}
+              height={400}
+              alt="Image of a building"
+            />
+          </motion.div>
+        </div>
       </div>
+      
     </>
   );
 };
