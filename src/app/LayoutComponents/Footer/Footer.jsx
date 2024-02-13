5a36a397af6d1cc570cb81abf6456ad8b3e9e1ca
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-14 00:20:19 +0530

    modified:   src/app/LayoutComponents/Footer/Footer.css 	modified:   src/app/LayoutComponents/Footer/Footer.jsx

--- src/app/LayoutComponents/Footer/Footer.jsx
+++ src/app/LayoutComponents/Footer/Footer.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import { SiInstagram } from "react-icons/si";
+import { LiaLinkedinIn } from "react-icons/lia";
+import { PiFigmaLogoDuotone } from "react-icons/pi";
+import { FaGithub } from "react-icons/fa6";
+import { neuo } from "@/app/font";
 const Footer = () => {
   return (
     <>
-    <div className="main__footer">
-      
-    </div>
+      <div className="main__footer">
+        <div className={` ${neuo.className} description__section`}>
+          <h3>shelke.prathamesh0012@gmail.com</h3>
+          <p>
+            "Don't hesitate to reach out—I'm currently open to both freelance
+            projects and job/internship opportunities."
+          </p>
+        </div>
+        <div className={` ${neuo.className} links__section`}>
+          <a href="https://www.instagram.com/praxx.dev/?igshid=ZGUzMzM3NWJiOQ%3D%3D">
+            <div className="instagram link">
+              <SiInstagram size={20} />
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/in/prathamesh-shelke/">
+            <div className="linkdin link">
+              <LiaLinkedinIn size={20} />
+            </div>
+          </a>
+          <a href="https://www.figma.com/@praxx">
+            <div className="figma link">
+              <PiFigmaLogoDuotone size={20} />
+            </div>
+          </a>
+          <a href="https://github.com/praxxOp">
+            <div className="github link">
+              <FaGithub size={20} />
+            </div>
+          </a>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
